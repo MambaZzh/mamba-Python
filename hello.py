7250a57061cb279222a125b55719fe50da0acb46
mambazzh <mambazzh@icloud.com>
2022-07-14 09:30:33 +0800

    hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+import keyword as kw
+
+print(kw.kwlist)
+
 #  字符串定义方式 ''、""、'''、"""
 str1 = "字符串1"
 print(str1)
@@ -19,7 +23,6 @@ str4 = """
 """
 print(str)
 
-
 #  转义字符\
 str5 = "转义字符\n转转转"
 print(str5)
@@ -37,3 +40,42 @@ z = "ABCDEFG"
 print(z[1:-1])
 print(z[2:4])
 print(z[1:3:2])
+
+if 3 > 1:
+    print("true")
+    print("yes")
+elif 3 > 2:
+    print("ture")
+else:
+    print("yes or no")
+
+x, y, z = "apple", "cherry", "banana"
+a = b = c = "Hi"
+
+def fun01():
+    #  定义一个局部变量x
+    x = "melon"
+    print("x局部变量：" + x)
+
+
+def fun02():
+    #  global 关键字，声明变量为 全局变量
+    global x
+    x = "melon2"
+    print("x全局变量：" + x)
+
+
+fun01()
+fun02()
+print("x全局变量：" + x)
+
+#  打印数据类型
+print(type(x))
+
+A = 10
+
+print(type(A))
+
+B = 1j
+
+print(type(B))
